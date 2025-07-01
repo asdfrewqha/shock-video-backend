5c149eedc371be930cda5e013619c142c6519de5
asdfrewqha <asdfrewqha@gmail.com>
2025-07-01 04:26:51 +0300

    return

--- fastapi_server/auth/token.py
+++ fastapi_server/auth/token.py
@@ -26,9 +26,9 @@ async def token(user: UserLogin):
         bd_user = await adapter.get_by_value(User, "username", user.identifier)
 
     if bd_user == [] or bd_user is None:
-        badresponse("User not found", 404)
-    else:
-        bd_user = bd_user[0]
+        return badresponse("User not found", 404)
+
+    bd_user = bd_user[0]
 
     if not Hasher.verify_password(user.password, bd_user.hashed_password):
         return badresponse("Forbidden", 403)
